dcb1ecfe14bd766a979bd1d05360ea64b316f61f
AgustinBoland <agustinboland67@gmail.com>
2023-03-17 19:03:23 -0300

    Se borro archivo basura

--- game.py
+++ game.py
@@ -1,16 +1,10 @@
 from random import choice, randrange
 from datetime import datetime
-import operator
 # Operadores posibles
 operators = ["+", "-", "*", "/"]
 
-ops = {
-    "+": operator.add,
-    "-": operator.sub,
-    "*": operator.mul,
-    "/": operator.truediv
-}   
 
+# a y b son los contadores que voy a utilizar para saber cuantos aciertos tuvo el usuario
 a = 0 
 b = 0
 
@@ -30,22 +24,29 @@ for i in range(0, times):
     # Le pedimos al usuario el resultado
     result = input("resultado: ")
     
-    op_char = operator
-    result1 = ops[op_char](number_1,number_2)
-    print (result1)
+    # Como los operadores son str, utilizo los if's para realizar un calculo auxiliar
+    # y compararlo con el resultado ingresado por el usuario
 
-    if float(result) == float(result1):
+    if operator == "+": 
+        result_aux = number_1 + number_2
+    elif operator == "-":
+        result_aux = number_1 - number_2
+    elif operator == "*":
+        result_aux = number_1 * number_2
+    else:
+        result_aux = number_1 / number_2
+
+    if float(result) == float(result_aux):
         print ("Correcto!")
         a += 1
     else:
         print ("incorrecto")
         b += 1
 # Al terminar toda la cantidad de cuentas por resolver.
-print (a,b)
 # Se vuelve a tomar la fecha y la hora.
 end_time = datetime.now()
 # Restando las fechas obtenemos el tiempo transcurrido.
 total_time = end_time - init_time
 # Mostramos ese tiempo en segundos.
 print(f"\n Tardaste {total_time.seconds} segundos.")
-print("Tuviste ", a , " aciertos y ", b, "resultados incorrectos")
+print("Tuviste", a , "aciertos y", b, "resultados incorrectos")
